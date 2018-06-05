fa97d7eda06614b6706e3e9665e00dc686828b7b
xiao-yan888 <1421918378@qq.com>
2018-06-05 13:32:59 +0800

    dierci

--- app/src/main/java/com/example/administrator/teamyikezhong/MainActivity.java
+++ app/src/main/java/com/example/administrator/teamyikezhong/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.administrator.teamyikezhong;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.administrator.teamyikezhong.fragment.ParagraphFragment;
+import com.example.administrator.teamyikezhong.fragment.PhotosFragment;
+import com.example.administrator.teamyikezhong.fragment.RecommendFragment;
+import com.example.administrator.teamyikezhong.fragment.VideoFragment;
 import com.hjm.bottomtabbar.BottomTabBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         mBtbar = (BottomTabBar) findViewById(R.id.btbar);
+        mBtbar.init(getSupportFragmentManager())
+                //.setImgSize(80,80)
+
+                .addTabItem(" ",R.drawable.tuijian2,R.drawable.tuijian1, RecommendFragment.class)
+                .addTabItem("  ",R.drawable.duanzi2,R.drawable.duanzi1, ParagraphFragment.class)
+                .addTabItem("   ",R.drawable.shipin2,R.drawable.shipin1, VideoFragment.class)
+               // .addTabItem("趣图",null, PhotosFragment.class)
+                .isShowDivider(false)
+                .setOnTabChangeListener(new BottomTabBar.OnTabChangeListener() {
+                    @Override
+                    public void onTabChange(int position, String name) {
+                    }
+                });
 
 
     }
---
+++ app/src/main/java/com/example/administrator/teamyikezhong/fragment/ParagraphFragment.java
@@ -0,0 +1,24 @@
+package com.example.administrator.teamyikezhong.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.teamyikezhong.R;
+
+/**
+ * Created by Administrator on 2018/6/5 0005.
+ */
+
+public class ParagraphFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.paragraphfragment, null);
+        return view;
+    }
+}
