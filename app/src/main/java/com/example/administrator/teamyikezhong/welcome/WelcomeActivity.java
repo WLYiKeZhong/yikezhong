da46cd54f731b647925e61c1e904ff3523dad497
Lan_haoyun <1654315871@qq.com>
2018-06-05 11:30:36 +0800

    倒计时

--- app/src/main/java/com/example/administrator/teamyikezhong/welcome/WelcomeActivity.java
+++ app/src/main/java/com/example/administrator/teamyikezhong/welcome/WelcomeActivity.java
@@ -1,15 +1,49 @@
 package com.example.administrator.teamyikezhong.welcome;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.administrator.teamyikezhong.MainActivity;
 import com.example.administrator.teamyikezhong.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    private ImageView mSplash;
+    /**
+     * 倒计时3秒
+     */
+    private TextView mDaojishi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initView();
+        //跳转主页面
+        final Intent intent=new Intent(WelcomeActivity.this, MainActivity.class);
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        };
+        timer.schedule(timerTask,3*1000);//此处3*1000代表3秒
+    }
+
+    /**
+     *初始化控件
+     */
+    private void initView() {
+        mSplash = (ImageView) findViewById(R.id.splash);
+
     }
 }
